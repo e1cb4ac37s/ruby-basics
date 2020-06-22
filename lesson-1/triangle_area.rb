c31d02781c9a8af7a197ac31cd83fb8468de7542
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-06-22 20:53:04 +0300

    lesson-1/triangle_area

---
+++ lesson-1/triangle_area.rb
@@ -0,0 +1,7 @@
+print 'Введите высоту треугольника: '
+height = gets.chomp
+
+print 'Введите основание треугольника: '
+base = gets.chomp
+
+puts "Площадь треугольника: #{0.5 * height.to_f * base.to_f}"
