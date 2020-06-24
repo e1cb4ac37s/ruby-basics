da288efe461ab34f9ffe91abbc1047ef3af707a2
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-06-24 19:17:05 +0300

    lesson-2/review-fixes yday.rb

--- lesson-2/yday.rb
+++ lesson-2/yday.rb
@@ -1,20 +1,17 @@
+# frozen_string_literal: true
+
 require './months'
 
-def leap_year? year
-  case 0
-  when year % 400
-    return true
-  when year % 100
-    return false
-  end
-  year % 4 == 0
+def leap_year?(year)
+  return true if (year % 400).zero?
+  return false if (year % 100).zero?
+
+  (year % 4).zero?
 end
 
-def yday day, month, year
+def yday(day, month, year)
   months = get_months(leap_year?(year))
-    .inject ([]) { |months_arr, (k, v)| months_arr << v }
-  months_days_sum = month.
-    times.
-    inject (0) { |months_days_sum, i| months_days_sum + months[i] }
+           .inject([]) { |months_arr, (_, v)| months_arr << v }
+  months_days_sum = month.times.inject(0) { |sum, i| sum + months[i] }
   months_days_sum - (months[month - 1] - day)
 end
